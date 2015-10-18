aeb30e61a82affa783e39b1389e42b0ef06b2cea
MrGlobi <raphael.schnider@hotmail.com>
2015-10-18 21:00:27 +0200

    version3

--- app/src/main/java/ch/ethz/inf/vs/vs_vvenzin_webservices/SOAPHttpClient.java
+++ app/src/main/java/ch/ethz/inf/vs/vs_vvenzin_webservices/SOAPHttpClient.java
@@ -3,19 +3,21 @@ package ch.ethz.inf.vs.vs_vvenzin_webservices;
 import android.util.Log;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
+import org.apache.http.client.HttpClient;
 
 /**
  * Created by Raphael on 17.10.2015.
  */
 public class SOAPHttpClient implements SimpleHttpClient {
-
-    String SOAP_ACTION =  "getSpot(Spot3)";
+    String NAMESPACE = "http://webservices.vslecture.vs.inf.ethz.ch/";
+    String SOAP_ACTION =  NAMESPACE + "getSpot";
     String URL = "http://vslab.inf.ethz.ch:8080/SunSPOTWebServices/SunSPOTWebservice";
     String SoapRequestXML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><S:Envelope xmlns:S=\"http://schemas.xmlsoap.org/soap/envelope/\">\n" +
             "    <S:Header/>\n" +
@@ -27,9 +29,10 @@ public class SOAPHttpClient implements SimpleHttpClient {
             "</S:Envelope>";
 
     public String execute(Object request){
+        HttpClient soapHttpClient = new DefaultHttpClient();
         // initialize HTTP post
         HttpPost httpPost = new HttpPost(URL);
-
+        String response = null;
 
         // load content to be sent
         StringEntity se = new StringEntity(SoapRequestXML, HTTP.UTF_8);
@@ -42,7 +45,7 @@ public class SOAPHttpClient implements SimpleHttpClient {
 
         HttpResponse httpResponse = null;
         try {
-            httpResponse = ((DefaultHttpClient)request).execute(httpPost);
+            httpResponse = soapHttpClient.execute(httpPost);
         } catch (Exception e) {
             Log.e("###", "Error sending SOAP request", e);
             e.printStackTrace();
@@ -52,7 +55,7 @@ public class SOAPHttpClient implements SimpleHttpClient {
 
         // get the response content
         HttpEntity httpEntity = httpResponse.getEntity();
-        String response = " ";
+
 
         try {
             response = EntityUtils.toString(httpEntity);
