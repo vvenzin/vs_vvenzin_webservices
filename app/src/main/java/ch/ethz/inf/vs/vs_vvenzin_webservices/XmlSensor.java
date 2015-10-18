80144fe6b099866dc0332d1b4693d657fdcdc2cb
eclGen <o.knueppel@gmail.com>
2015-10-18 22:14:57 +0200

    Fix Task 2

--- app/src/main/java/ch/ethz/inf/vs/vs_vvenzin_webservices/XmlSensor.java
+++ app/src/main/java/ch/ethz/inf/vs/vs_vvenzin_webservices/XmlSensor.java
@@ -1,18 +1,13 @@
 package ch.ethz.inf.vs.vs_vvenzin_webservices;
 
 
-import java.io.StringReader;
-
-import org.xmlpull.v1.XmlPullParser;
-
-import org.kxml2.io.KXmlParser;
-
 import android.util.Log;
 
+import org.kxml2.io.KXmlParser;
+import org.xmlpull.v1.XmlPullParser;
 
-/**
- * Created by Raphael on 14.10.2015.
- */
+import java.io.StringReader;
+
 public class XmlSensor extends AbstractSensor  {
 
     String NAMESPACE = "http://webservices.vslecture.vs.inf.ethz.ch/";
@@ -27,10 +22,11 @@ public class XmlSensor extends AbstractSensor  {
             while (eventType != XmlPullParser.END_DOCUMENT){
                 if(eventType == XmlPullParser.START_TAG){
                     if(xp.getName().equals("temperature")){
-                        eventType=xp.next();
-                        stemperature= xp.getText();
+                        eventType = xp.next();
+                        stemperature  = xp.getText();
                         break;
                     }
+                    else eventType = xp.next();
                 }
                 else eventType = xp.next();
             }
